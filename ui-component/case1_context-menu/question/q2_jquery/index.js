08a5861bad888554ff10713200e3c391f386971c
mjjomdev <mjjom.dev@gmail.com>
2021-05-04 01:42:49 +0900

    ADD: 제이쿼리로 구현

--- ui-component/case1_context-menu/question/q2_jquery/index.js
+++ ui-component/case1_context-menu/question/q2_jquery/index.js
@@ -3,3 +3,28 @@ import "./style.css";
 import $ from "jquery";
 
 // Write JQuery code here!
+const $wrapper = $('.wrapper');
+const $items = $wrapper.find('.item');
+const $body = $('body');
+
+// method 1
+
+// $wrapper.on('click', '.item', function(e){
+//   e.stopPropagation();
+//   $(this).toggleClass('open').siblings().removeClass('open');
+// })
+
+// $body.on('click', function(e){
+//   $items.removeClass('open');
+// })
+
+
+// method 2
+$body.on('click', function(e){
+  const item = $(e.target);
+  if (item.is('.item')){
+    item.toggleClass('open').siblings().removeClass('open');
+  } else {
+    $items.removeClass('open');
+  }
+})
