65b6199afb73ff9e26871cb2429631f69b0b5f0f
mjjomdev <mjjom.dev@gmail.com>
2021-05-04 01:29:33 +0900

    ADD: 바닐라 자바스크립트로 메뉴 구현

--- ui-component/case1_context-menu/question/q1_js/index.js
+++ ui-component/case1_context-menu/question/q1_js/index.js
@@ -1,1 +1,56 @@
+// Import stylesheets
+import "./style.css";
+
 // Write Javascript code here!
+// 1. 초보자의 접근법
+// const items = document.querySelectorAll('.item');
+// items.forEach(function(item) {
+//   item.addEventListener('click', function(e) {
+//     item.classList.toggle('open');
+//     items.forEach(function(elem) {
+//       if (elem !== item) elem.classList.remove('open');
+//     });
+//   });
+// });
+
+
+
+// 2. 더 나은 방법 (1)
+// const wrapper = document.querySelector('.wrapper');
+// const items = document.querySelectorAll('.item');
+// wrapper.addEventListener('click', function(e) {
+//   const targetElem = e.target;
+//   e.stopPropagation();
+//   if (!targetElem.classList.contains('item')) return;
+//   targetElem.classList.toggle('open');
+//   items.forEach(function(elem) {
+//     if (elem !== targetElem) elem.classList.remove('open');
+//   });
+// });
+// document.body.addEventListener('click', function(e) {
+//   if (e.target.classList.contains('context')) return;
+//   items.forEach(function(elem) {
+//     elem.classList.remove('open');
+//   });
+// });
+
+
+
+// 3. 더 나은 방법 (2)
+const wrapper = document.querySelector('.wrapper');
+const items = document.querySelectorAll('.item');
+
+document.body.addEventListener('click', function(e) {
+  const targetClassList = e.target.classList;
+  if (targetClassList.contains('context')) return;
+  if (targetClassList.contains('item')) {
+    targetClassList.toggle('open');
+    items.forEach(function(elem) {
+      if (elem !== e.target) elem.classList.remove('open');
+    });
+    return;
+  }
+  items.forEach(function(elem) {
+    elem.classList.remove('open');
+  });
+});
