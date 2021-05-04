d5435cd25107da89540dfdf2bc2b0c85896e5cc8
mjjomdev <mjjom.dev@gmail.com>
2021-05-05 01:12:33 +0900

    ADD: html details 태그 활용

--- ui-component/case01_context-menu/question/q3_html-js/index.js
+++ ui-component/case01_context-menu/question/q3_html-js/index.js
@@ -2,3 +2,16 @@
 import "./style.css";
 
 // Write Javascript code here!
+const details = document.querySelectorAll('details');
+document.body.addEventListener('click', function (e) {
+  if (e.target.nodeName !== "SUMMARY" && e.target.nodeName !== "P") {
+    details.forEach(function (detail) {
+      detail.removeAttribute('open');
+    })
+  }
+  details.forEach(function (detail) {
+    if (detail !== e.target.parentElement) {
+      detail.removeAttribute('open');
+    }
+  })
+})
